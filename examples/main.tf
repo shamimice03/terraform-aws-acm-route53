f78ee3045fc35263e2fa378fb5f904c25a1645ce
Md Shamim <shamimice03@gmail.com>
2023-03-06 23:33:21 +0900

    example added

--- examples/main.tf
+++ examples/main.tf
@@ -1,15 +1,15 @@
 module "acm" {
 
-source = "github.com/shamimice03/terraform-aws-acm-route53"
+    source = "github.com/shamimice03/terraform-aws-acm-route53"
 
-domain_name = "webapp.example.com"
-validation_method = "DNS"
-hosted_zone_name = "example.com"
-private_zone = false
-allow_record_overwrite = true
-ttl = 60
-tags = {
-    "Name" = "ssl-cert"
-}
+    domain_name = "webapp.example.com"
+    validation_method = "DNS"
+    hosted_zone_name = "example.com"
+    private_zone = false
+    allow_record_overwrite = true
+    ttl = 60
+    tags = {
+        "Name" = "ssl-cert"
+    }
 
 }
